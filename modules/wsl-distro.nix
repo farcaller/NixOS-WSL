87022b2b73810c0e3ecbcd78b5403f9e348baca9
Vladimir Pouzanov <farcaller@gmail.com>
2023-11-17 18:04:44 +0000

    Remove opengl

--- modules/wsl-distro.nix
+++ modules/wsl-distro.nix
@@ -69,7 +69,7 @@ in
     # WSL does not support virtual consoles
     console.enable = false;
 
-    hardware.opengl.enable = true; # Enable GPU acceleration
+    hardware.opengl.enable = false; # Enable GPU acceleration
 
     environment = {
       # Only set the options if the files are managed by WSL
